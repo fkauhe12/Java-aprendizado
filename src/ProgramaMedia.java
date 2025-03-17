f9136108b10aef1ae7dd2df8e04255a751540c0d
fkauhe12 <fkauhe12@gmail.com>
2025-03-17 08:57:28 -0300

    Adicionar lógica de aprovação e reprovação no ProgramaMedia

--- src/ProgramaMedia.java
+++ src/ProgramaMedia.java
@@ -5,16 +5,22 @@ public class ProgramaMedia {
     try (Scanner ler = new Scanner(System.in)) {
       float media;
       
-      System.out.println("Digite seu nome: ");
+      System.out.print("Digite seu nome: ");
       String nome = ler.nextLine();
-      System.out.println("Digite primeira nota: ");
+      System.out.print("Digite primeira nota: ");
       float nota1 = ler.nextFloat();
-      System.out.println("Digite segunda nota: ");
+      System.out.print("Digite segunda nota: ");
       float nota2 = ler.nextFloat();
       
       media = (nota1 + nota2) / 2;
       
       System.out.println(nome +" sua media é "+ media);
+      
+      if(media >= 6){
+          System.out.println("Aprovado");
+      }else{
+           System.out.println("Reprovado");
+      }
     }
   }
 }
